5117ba984f116d7ea1796e9f72e88ed4d13b8e2c
Sanjin84 <ozrobotix@gmail.com>
2023-10-11 20:53:05 +1100

    CORS specific origins added

--- main.py
+++ main.py
@@ -40,9 +40,16 @@ class TeamsInput(BaseModel):
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://vccfinal.com",
+    "https://vccfinal.com:8000"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
