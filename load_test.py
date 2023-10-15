be8de0f2580f0b77aac3409f0878466d97adb43d
DawoodLaiq <86498931+DawoodLaiq@users.noreply.github.com>
2023-10-16 02:55:30 +0500

    tests

---
+++ load_test.py
@@ -0,0 +1,57 @@
+from locust import HttpUser, task, between
+
+#pip install locust
+#locust -f load_test.py
+#localhost:8089
+
+class UserBehavior(HttpUser):
+    wait_time = between(1, 2)
+
+    team_data = {
+        "teams": [
+      "RedWolves",
+      "BlueSharks",
+      "ThunderBolts",
+      "QuickFoxes",
+      "CyberKnights",
+      "DataMiners",
+      "SkyEagles",
+      "SeaLions",
+      "MountainGoats",
+      "DarkHorses",
+      "SilverHawks",
+      "GoldenBears",
+      "SwiftCats",
+      "MightyOwls",
+      "FrostWolves",
+      "SolarFlares",
+      "MoonWalkers",
+      "StarFighters",
+      "WindRiders",
+      "FirePhoenix"
+    ]
+    }
+
+    @task(1)
+    def get_comp_table(self):
+        self.client.get("/get_comp_table")
+    
+    @task(2)
+    def get_manual_questions(self):
+        self.client.get("/get_manual_questions")
+
+    @task(4)
+    def get_questions_for_teams(self):
+        for team_name in self.team_data["teams"]:
+            self.client.get(f"/questions/{team_name}")
+    
+    @task(5)
+    def submit_answer(self):
+     
+        for team_name in self.team_data["teams"]:
+            payload = {
+                "id": "1",
+                "answer": "b",
+                "team_name": team_name
+            }
+            self.client.post("/submit_mcqs_answer", json=payload)
