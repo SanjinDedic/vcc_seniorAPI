0a95e0c88daf451e1ddc5e73f306302df1c14fd0
DawoodLaiq <dawoodhassan12@gmail.com>
2024-09-25 22:28:14 +0500

    added update questions endpoint

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 CURRENT_DIR = os.path.dirname(__file__)
 CURRENT_DB = os.path.join(CURRENT_DIR, "initial.db")
+BACKUP_DIR = os.path.join(CURRENT_DIR, "backup")
